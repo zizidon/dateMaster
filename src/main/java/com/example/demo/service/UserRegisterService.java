8a12d380ef11f73052d2af19f8838f313e4ffda0
aoya <20223142@kcska.onmicrosoft.com>
2024-12-18 11:03:57 +0900

    新規登録の作成途中

--- src/main/java/com/example/demo/service/UserRegisterService.java
+++ src/main/java/com/example/demo/service/UserRegisterService.java
@@ -4,20 +4,24 @@ import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.demo.repository.UserRepository;
+
 public class UserRegisterService {
 
 	@Autowired
 	HttpSession session;
 
-	public boolean register(String password, String confirmPassword) {
+	@Autowired
+	UserRepository userRepo;
 
-		session.setAttribute("password", password);
-		session.setAttribute("confirmPassword", confirmPassword);
+	public boolean register(String id, String name, String password, String confirmPassword) {
 
 		//パスワード確認
 		if (password.equals(confirmPassword)) {
+			System.out.println("一致");
 			return true;
 		} else {
+			System.out.println("不一致");
 			return false;
 		}
 	}
