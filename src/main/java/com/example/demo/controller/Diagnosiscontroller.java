67d437eb28e1fae592312d3f60adc4a1c9ae3df9
aoya <20223142@kcska.onmicrosoft.com>
2025-01-15 17:21:12 +0900

    自己診断画面への遷移

--- src/main/java/com/example/demo/controller/Diagnosiscontroller.java
+++ src/main/java/com/example/demo/controller/Diagnosiscontroller.java
@@ -7,13 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/dateMaster")
 public class Diagnosiscontroller {
-	
-	@GetMapping("/diagnosis")
+
+	//自己診断一覧画面へ遷移
+	@GetMapping("/diagonosis")
 	public String showDiagnosisPage() {
-		
+
 		//diagnosis.htmlを返す
-		
-		return "diagnosis/diagnosis";
+
+		return "diagonosis/diagonosis";
 	}
 
+	//自己診断回答画面へ遷移
+	@GetMapping("/self_diagonosis")
+	public String showSelfDiagnosisPage() {
+		// self_diagnosis.htmlを返す
+		return "self_diagonosis/self_diagonosis";
+	}
 }
