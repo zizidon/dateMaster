7dcf4cf7c94b0ed613936de89b5c8551a06bf1c0
sakata <sakata@kcs>
2025-01-20 16:05:31 +0900

    プラン作成保存のエンティティとリポジトリー

--- src/main/java/com/example/demo/entity/DateShare.java
+++ src/main/java/com/example/demo/entity/DateShare.java
@@ -1,5 +1,19 @@
 package com.example.demo.entity;
 
-public class DateShare {
+import org.springframework.data.annotation.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DateShare {
+    @Id
+    private Long plans_id;  // 自動採番
+    private Long spot1;  // スポット1のID
+    private Long spot2;  // スポット2のID
+    private Long spot3;  // スポット3のID
+    private Integer count;  // プランの使用回数
 }
--- src/main/java/com/example/demo/repository/DateShareRepository.java
+++ src/main/java/com/example/demo/repository/DateShareRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public interface DateShareRepository {
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entity.DateShare;
+
+public interface DateShareRepository extends CrudRepository<DateShare, Long> {
+    // spot1, spot2, spot3が一致するプランを検索
+    DateShare findBySpot1AndSpot2AndSpot3(Long spot1, Long spot2, Long spot3);
 }
